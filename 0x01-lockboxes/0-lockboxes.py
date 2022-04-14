566c3d85ef3cc6ba53f797ed4fe525183396f762
Camella Enyi <enyicamella@gmail.com>
2022-04-14 20:58:27 +0100

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,36 +1,42 @@
 #!/usr/bin/python3
 """
-Module 0-lockboxes
+We have n number of locked boxes in front of us.
+Each box is numbered sequentially from 0 to n - 1
+and each box may contain keys to the other boxes.
 """
 
 
 def canUnlockAll(boxes):
-    """
-    You have n number of locked boxes in front of you.
-    Each box is numbered sequentially from 0 to n - 1,
-    and each box may contain keys to the other boxes.
-    Determine if all the boxes can be opened.
-    """
+    """a method that determines if all the boxes can be opened"""
+    """ edge cases commented out """
     if boxes is None:
         return False
-    if len(boxes) == 1:
+    if not isinstance(boxes, list):
+        return False
+    if boxes == []:
+        return False
+    if boxes == [[]]:
         return True
-    # Track the visited boxes
-    visited = set()
-    # The first box is unlocked
-    visited.add(0)
-
-    # Use a stack to do a Depth First Search
-    stack = []
-    stack.append(0)
-
-    while stack:
-        keys = boxes[stack.pop()]
-        for key in keys:
-            if key not in visited:
-                visited.add(key)
-                stack.append(key)
-    # All have been visited if the
-    # number of boxes is equal to the
-    # number of visited boxes
-    return len(boxes) == len(visited)
+    if boxes[0] == [] and len(boxes) > 1:
+        return False
+    for i in boxes:
+        if not isinstance(i, list):
+            return False
+        else:
+            for item in i:
+                if not isinstance(item, int):
+                    return False
+    Unlocked = [False for i in boxes]
+    Unlocked[0] = True
+    Open = [False for i in boxes]
+    while [True for i in range(len(boxes))
+           if Open[i] is False and Unlocked[i] is True]:
+        for k in range(len(boxes)):
+            if Unlocked[k] is True:
+                Open[k] = True
+                for i in boxes[k]:
+                    try:
+                        Unlocked[i] = True
+                    except:
+                        pass
+    return False not in Unlocked
